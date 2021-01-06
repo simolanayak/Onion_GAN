3bdba2cd4578581f8f6f4d9ec7ae90f84251099f
Simola Nayak <simola.nayak@gmail.com>
2021-01-06 10:59:27 -0800

    added onion spider

--- data_gathering/article_gatherer/article_gatherer/spiders/onionspider.py
+++ data_gathering/article_gatherer/article_gatherer/spiders/onionspider.py
@@ -2,14 +2,14 @@ import scrapy
 from scrapy import linkextractors
 
 class OnionSpider(scrapy.Spider):
-    name = 'onion-arts'
-    allowed_domains = ['https://politics.theonion.com']
-    rules = [
-        Rule(LinkExtractor(allow=()))
-    ]
+    name = "onionspider"
+    start_urls = ['https://politics.theonion.com', 'https://entertainment.theonion.com', 'https://ogn.theonion.com', 'https://sports.theonion.com']
+    ##Every Onion Article
     
     def parse(self, response):
+        
         page = response.url.split('/')[-1]
+        filename = 'article-%.html' % page
         #TODO: page title is basically article title
-        with open(filename, 'wb') as f:
-            f.write("""whatever the article is""")
+        with open(filename, 'wb') as f:s
+            f.write(response.body)
